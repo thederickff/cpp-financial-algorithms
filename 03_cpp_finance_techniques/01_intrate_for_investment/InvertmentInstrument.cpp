a98bf2c35dd7f52bbea0910578c3c72090806f5d
derickfelix <derickfelix4321@gmail.com>
2020-03-31 13:00:16 -0400

    Add InvestmentInstrument

---
+++ 03_cpp_finance_techniques/01_intrate_for_investment/InvertmentInstrument.cpp
@@ -0,0 +1,80 @@
+#include "InvestmentInstrument.h"
+
+BondInstrument::BondInstrument(double principal, double monthlyPayment)
+: m_principal(principal), m_monthlyPayment(monthlyPayment)
+{
+
+}
+
+BondInstrument::BondInstrument(const BondInstrument &v)
+: m_principal(v.m_principal), m_monthlyPayment(v.m_monthlyPayment)
+{
+
+}
+
+BondInstrument &BondInstrument::operator=(const BondInstrument &v)
+{
+  if (this != &v)
+  {
+    this->m_principal = v.m_principal;
+    this->m_monthlyPayment = v.m_monthlyPayment;
+  }
+
+  return *this;
+}
+
+BondInstrument::~BondInstrument()
+{
+
+}
+
+double BondInstrument::getPrincipal()
+{
+  return m_principal;
+}
+
+double BondInstrument::getMonthlyPayment()
+{
+  return m_monthlyPayment;
+}
+
+
+MortgageInstrument::MortgageInstrument(double monthlyPay, double propertyValue, double downPayment)
+: m_monthlyPay(monthlyPay), m_propertyValue(propertyValue), m_downPayment(downPayment)
+{
+
+}
+
+MortgageInstrument::MortgageInstrument(const MortgageInstrument &v)
+: m_monthlyPay(v.m_monthlyPay), m_propertyValue(v.m_propertyValue), m_downPayment(v.m_downPayment)
+{
+
+}
+
+MortgageInstrument &MortgageInstrument::operator=(const MortgageInstrument &v)
+{
+  if (this != &v)
+  {
+    this->m_monthlyPay = v.m_monthlyPay;
+    this->m_propertyValue = v.m_propertyValue;
+    this->m_downPayment = v.m_propertyValue;
+  }
+
+  return *this;
+}
+
+MortgageInstrument::~MortgageInstrument()
+{
+
+}
+
+
+double MortgageInstrument::getPrincipal()
+{
+  return m_propertyValue - m_downPayment;
+}
+
+double MortgageInstrument::getMonthlyPayment()
+{
+  return m_monthlyPay;
+}
---
+++ 03_cpp_finance_techniques/01_intrate_for_investment/InvestmentInstrument.h
@@ -0,0 +1,39 @@
+#ifndef __FinancialSamples_InvestmentInstrument__
+#define __FinancialSamples_InvestmentInstrument__
+
+class BondInstrument
+{
+public:
+  BondInstrument() {}
+  BondInstrument(double principal, double monthlyPayment);
+  BondInstrument(const BondInstrument &v);
+  BondInstrument &operator=(const BondInstrument &v);
+  ~BondInstrument();
+
+  double getPrincipal();
+  double getMonthlyPayment();
+
+private:
+  double m_principal;
+  double m_monthlyPayment;
+};
+
+class MortgageInstrument
+{
+public:
+  MortgageInstrument() {}
+  MortgageInstrument(double monthlyPay, double propertyValue, double downPayment);
+  MortgageInstrument(const MortgageInstrument &v);
+  MortgageInstrument &operator=(const MortgageInstrument &v);
+  ~MortgageInstrument();
+
+  double getPrincipal();
+  double getMonthlyPayment();
+  
+private:
+  double m_monthlyPay;
+  double m_propertyValue;
+  double m_downPayment;
+};
+
+#endif /* defined(__FinancialSamples_InvestmentInstrument__) */
